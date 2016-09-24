ced5405f37b99bfa2b84dbfac50d80e485275ae7
Nick <nickpourquie@gmail.com>
2016-09-24 15:37:18 +0200

    Fixed formatting.

--- ProjectPrefire/Analyzer.cs
+++ ProjectPrefire/Analyzer.cs
@@ -7,46 +7,47 @@ using System.IO;
 using DemoInfo;
 using System.Windows.Forms;
 using System.Drawing;
+
 namespace ProjectPrefire
 {
-    class Analyzer
-    {
-        //Logger log = new Logger();
-        Drawer drawer { get; set; }
+	class Analyzer
+	{
+		//Logger log = new Logger();
+		Drawer drawer { get; set; }
+
 		Game game;
 
-		public Analyzer(Game game)
-        {
+		public Analyzer (Game game)
+		{
 			this.game = game;
-            Control canvas = Application.OpenForms[0].Controls.Find("mapBox", true).FirstOrDefault();
-            drawer = new Drawer(canvas);
-            Filter();
-        }
+			Control canvas = Application.OpenForms [0].Controls.Find ("mapBox", true).FirstOrDefault ();
+			drawer = new Drawer (canvas);
+			Filter ();
+		}
 
-        public void Filter(int delay = 3)
-        {
-			foreach (Player player in game.players)
-            {
+		public void Filter (int delay = 3)
+		{
+			foreach (Player player in game.players) {
 
 
-				foreach(PlayerState state in player.playerStates){
+				foreach (PlayerState state in player.playerStates) {
 					Color color = Color.Blue;
 					if (player.team == 1) {
 						color = Color.Red;
 					}
 
-					float x2 = state.posX + 25 * (float)Math.Cos((float)(Math.PI / 180) * state.angX *-1);
-					float y2 = state.posY + 25 * (float)Math.Sin((float)(Math.PI / 180) * state.angY*-1);
+					float x2 = state.posX + 25 * (float)Math.Cos ((float)(Math.PI / 180) * state.angX * -1);
+					float y2 = state.posY + 25 * (float)Math.Sin ((float)(Math.PI / 180) * state.angY * -1);
 
-					drawer.drawCircle(state.posX, state.posY, color);
-					drawer.drawLine(state.posX, state.posY,x2,y2, color);
-                }
+					drawer.drawCircle (state.posX, state.posY, color);
+					drawer.drawLine (state.posX, state.posY, x2, y2, color);
+				}
 
 				/*
                 drawer.drawString("HIE asdasd1 123 AD dd", 10, 10 * id, Color.Red);
                 drawer.drawRectangle(10, 10, 100, 100, Color.Red);
           		*/
-            }
-        }
+			}
+		}
 	}
 }
